2b5167bd899a05cb916f42bd914896d2d11a5ab6
manouAlda <lenepveumanou@gmail.com>
2023-09-29 10:47:07 +0300

    Fonction bibliothèque

--- var.c
+++ var.c
@@ -17,3 +17,37 @@ int EstPoints(char* tab){
 			if(tab[j]==':' && tab[j+1]!=':')	v++;               							
 	return v;
 }
+
+int count_line (FILE* f){
+	f = fopen ("/var/log/auth.log","r");
+	char* tab = malloc(300);
+	int total=0;
+	while(fgets(tab,300,f)){
+		total++;
+	}
+	fclose(f);
+	return total;
+}
+
+void freePasswd (char* contenu, char* user, char* mot, char* rep){
+	free(contenu);
+	free(user);
+	free(rep);
+	free(mot);
+}
+
+void freedom (char* line,char* month, char* day, char* time, char* session, char*login,char* hostname,char* service, char* action, char* infos){
+
+	free(line);
+	free(month);
+	free(day);
+	free(time);
+	free(session);
+	free(login);
+	free(hostname);
+	free(service);
+	free(action);
+	free(infos);
+
+}
+
--- var.h
+++ var.h
@@ -1,7 +1,11 @@
 #ifndef VAR_H
 #define VAR_H
 
+#include <stdio.h>
+
 char** allouer(int line,int cols);
 int EstPoints(char* tab);
-
+int count_line (FILE* f);
+void freePasswd (char* contenu, char* user, char* mot, char* rep);
+void freedom (char* line,char* month, char* day, char* time, char* session, char*login,char* hostname,char* service, char* action, char* infos);
 #endif
